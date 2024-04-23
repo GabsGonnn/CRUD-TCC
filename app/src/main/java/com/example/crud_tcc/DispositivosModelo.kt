7bba9ec886b159fb3301afd50a96e5f37f1e77ca
GabsGonnn <gabriel.goncalves.2006@gmail.com>
2024-04-23 17:05:05 -0300

    Insertion

---
+++ app/src/main/java/com/example/crud_tcc/DispositivosModelo.kt
@@ -0,0 +1,11 @@
+package com.example.crud_tcc
+
+class DispositivosModelo(
+    var dispId: String? = null,
+    var dispNome: String? = null,
+    var dispTipo: String? = null,
+    var dispStatus: String? = null,
+    var dispLocal: String? = null,
+    var dispDtInst: String? = null,
+    var dispDtAtt: String? = null
+)
